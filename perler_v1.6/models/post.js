ff899d09f7df0a361658296d3bcd48c368c19183
Creative Shin <creative-shin@outlook.com>
2017-12-29 11:02:35 +0000

    admin paned

---
+++ perler_v1.6/models/post.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var postSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+module.exports = mongoose.model("Post", postSchema);
